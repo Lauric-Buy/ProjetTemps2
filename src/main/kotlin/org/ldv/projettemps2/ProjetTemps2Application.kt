b04e990ea53d4651eff6f4b0099ad3630a5aca7b
Lauric-Buy <toons05072004@gmail.com>
2025-11-13 10:53:35 +0100

    Initial commit

---
+++ src/main/kotlin/org/ldv/projettemps2/ProjetTemps2Application.kt
@@ -0,0 +1,11 @@
+package org.ldv.projettemps2
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjetTemps2Application
+
+fun main(args: Array<String>) {
+    runApplication<ProjetTemps2Application>(*args)
+}
---
+++ src/test/kotlin/org/ldv/projettemps2/ProjetTemps2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package org.ldv.projettemps2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ProjetTemps2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
